0b195ed22f2b790f208dc6d7f24c9f463f7012d0
Danny Bendelman <bendelman@gmail.com>
2018-08-12 23:32:29 -0400

    Implemented deployment script

--- flask/deploy/deploy.sh
+++ flask/deploy/deploy.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 set -ex
 
-image_name=$REGISTRY_SERVER/dbendelman/notejam
-# not yet implemented...
+# Find all app instance IDs:
+target_group="$(aws elbv2 describe-target-groups --names "notejam" | jq -r '.TargetGroups[0].TargetGroupArn')"
+instance_ids=( $(aws elbv2 describe-target-health --target-group-arn "$target_group" | jq -r '.TargetHealthDescriptions | map(.Target.Id) | join(" ")') )
+instance_count=${#instance_ids[@]}
+
+for instance_id in "${instance_ids[@]}"; do
+
+    # Get the instance's DNS:
+	instance_dns="$(aws ec2 describe-instances --instance-ids "$instance_id" | jq -r '.Reservations[0].Instances[0].PublicDnsName' 2>/dev/null)"
+	[ -n "$instance_dns" ] ||
+		( echo >&2 "FATAL: Failed to retrieve the public DNS of instance $instance_id."; return 1 )
+
+    # Deploy the app:
+    echo ">>> Deploying app on $instance_id ( $instance_dns )"
+    ssh \
+        -o "StrictHostKeyChecking no" \
+        -o "UserKnownHostsFile /dev/null" \
+        ubuntu@$instance_dns \
+            deploy
+
+done
